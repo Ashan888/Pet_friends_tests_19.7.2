5576e2027d2dac6b6787d45b1770e866b447ca30
Ashan888 <888grey.rus@mail.ru>
2022-11-07 19:49:23 +0300

    first_commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+#auth_key = '0d0851683b54b86aae1d3a969435a99cb4de0cfb454b9639a4d56ea4'
+valid_email = "888grey.rus@mail.ru"
+valid_password = "123121pf"
+no_valid_email = "1234fdh"
+no_valid_password = "___432gg@h"
+valid_email1 = "no_valid@emai.com"
+valid_password1 = "ne_parol"
+name = 'test'
+animal_type = ''
+age = '199'
